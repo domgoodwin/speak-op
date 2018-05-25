91712ab0dd8733bc010e9c34b97e405ca9c75a0b
Goodwin, Dom <dom.goodwin@capgemini.com>
2018-05-25 11:35:14 +0100

    Auto deploy script and refactor

---
+++ Alexa/Lambda.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+variable "app_version" {}
+
+resource "aws_lambda_function" "example" {
+  function_name = "SpeakOp_Main"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "speakop-lambda-deploy"
+  s3_key    = "package-${var.app_version}.zip"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "main.handler"
+
+  runtime = "nodejs6.10"
+
+  role = "${aws_iam_role.lambda_exec.arn}"
+}
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
