8913e279bfa26a5d3e8664497b0e1df3061f7afe
Olivia Minshall <olivia.minshall@capgemini.com>
2018-06-11 14:46:09 +0100

    updated scripts

--- Dashboard/app/routes/startup.js
+++ Dashboard/app/routes/startup.js
@@ -1,19 +1,26 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const shell = require('node-powershell');
+app.use(bodyParser.urlencoded({   extended: true })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
 // routes/note_routes.js
 module.exports = function(app, db) {
-  app.post('/startup', (req, res) => {
+  app.post('/startup', function(req, res) {
     var spawn = require("child_process").spawn,child;
-    child = spawn("powershell.exe",["C:/\\repo/\\speak-op/\\Dashboard/\\Kiosk-Mode/\\Ciklum-Kiosk.ps1"]);
-    child.stdout.on("data",function(data){
-        console.log("Powershell Data: " + data);
+    let ps = new shell({
+      executionPolicy: 'Bypass',
+      noProfile: true
     });
-    child.stderr.on("data",function(data){
-        console.log("Powershell Errors: " + data);
+    ps.addCommand('C:/\\repo/\\speak-op/\\Dashboard/\\Kiosk-Mode/\\Ciklum-Kiosk.ps1')
+    ps.invoke()
+    .then(output => {
+      console.log(output);
+    })
+    .catch(err => {
+      console.log(err);
+      ps.dispose();
     });
-    child.on("exit",function(){
-        console.log("Powershell Script finished");
-        res.send('Start-up script successfully ran.');
-    });
-    child.stdin.end(); //end input
-    console.log(req.body)
+    res.send('Start-up script triggered.');
   });
 };
